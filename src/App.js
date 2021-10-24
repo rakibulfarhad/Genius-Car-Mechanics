4a128e5e45824d5a38d90c73d5caf52a9e881ff3
rakibulfarhad <mraisfa@gmail.com>
2021-10-24 19:50:32 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import AuthProvider from './contexts/AuthProvider';
+import Booking from './Pages/Booking/Booking/Booking';
 import Home from './Pages/Home/Home/Home';
+import Login from './Pages/Login/Login/Login';
+import PrivateRoute from './Pages/Login/PrivateRoute/PrivateRoute';
+import NotFound from './Pages/NotFound/NotFound';
+import Header from './Pages/Shared/Header/Header';
 
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <AuthProvider>
+        <Router>
+          <Header></Header>
+          <Switch>
+            <Route exact path="/">
+              <Home></Home>
+            </Route>
+            <Route exact path="/home">
+              <Home></Home>
+            </Route>
+            <Route exact path="/login">
+              <Login></Login>
+            </Route>
+            <PrivateRoute exact path="/booking/:serviceId">
+              <Booking></Booking>
+            </PrivateRoute>
+            <Route exact path="*">
+              <NotFound></NotFound>
+            </Route>
+          </Switch>
+        </Router>
+      </AuthProvider>
     </div>
   );
 }
